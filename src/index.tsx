5c8546a219076ff0981384b70459448cd9392973
Sam Svindland <svindland@Sams-MacBook-Pro.local>
2021-01-23 13:12:05 -0800

    add service woker

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import "./styles/index.css";
 
+import * as registerServiceWorker from "./registerServiceWorker";
+
 import App from "./components/App";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -16,3 +18,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+registerServiceWorker.register();
